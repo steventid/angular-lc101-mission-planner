417d41b8b52c631a7d102e3c3c94adc042471067
Steven Tidwell <steventid1313@gmail.com>
2022-09-29 14:08:57 -0500

    Completed part 2 of Studio 3

--- src/app/crew/crew.component.ts
+++ src/app/crew/crew.component.ts
@@ -25,5 +25,13 @@ export class CrewComponent implements OnInit {
   ngOnInit() { }
 
   // Code the 'addCrewMember' function here:
-
+  addCrewMember(member: object) {
+    if (this.crew.includes(member)) {
+      this.crew.splice(this.crew.indexOf(member),1);
+      return;
+    }
+    if (this.crew.length < 3) {
+      this.crew.push(member);
+    }
+  }
 }
