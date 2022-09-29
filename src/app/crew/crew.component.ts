0dacbcf4410c5793f8803e537e55cc40f4bf348c
Steven Tidwell <steventid1313@gmail.com>
2022-09-29 16:40:31 -0500

    Completed all bonus missions

--- src/app/crew/crew.component.ts
+++ src/app/crew/crew.component.ts
@@ -11,13 +11,13 @@ export class CrewComponent implements OnInit {
   crew: object[] = [];
 
   candidates: object[] = [
-    {name: "Sally Ride", photo: 'https://handlers.education.launchcode.org/static/images/sally-ride.jpg'},
-    {name: "Mae Jemison", photo: 'https://handlers.education.launchcode.org/static/images/mae-jemison.jpg'},
-    {name: "Ellen Ochoa", photo: 'https://handlers.education.launchcode.org/static/images/ellen-ochoa.jpg'},
-    {name: "Frederick Gregory", photo: 'https://handlers.education.launchcode.org/static/images/frederick-gregory.jpg'},
-    {name: "Guion Bluford", photo: 'https://handlers.education.launchcode.org/static/images/guion-bluford.jpg'},
-    {name: "Kjell Lindgren", photo: 'https://handlers.education.launchcode.org/static/images/kjell-lindgren.jpg'},
-    {name: "Jeanette Epps", photo: 'https://handlers.education.launchcode.org/static/images/jeanette-epps.jpg'}
+    {name: "Sally Ride", photo: 'https://handlers.education.launchcode.org/static/images/sally-ride.jpg', born: 'May 26, 1951', died: 'July 23, 2012'},
+    {name: "Mae Jemison", photo: 'https://handlers.education.launchcode.org/static/images/mae-jemison.jpg', born: 'October 17, 1956', died: 'N/A'},
+    {name: "Ellen Ochoa", photo: 'https://handlers.education.launchcode.org/static/images/ellen-ochoa.jpg', born: 'May 10, 1958', died: 'N/A'},
+    {name: "Frederick Gregory", photo: 'https://handlers.education.launchcode.org/static/images/frederick-gregory.jpg', born: 'January 7, 1941', died: 'N/A'},
+    {name: "Guion Bluford", photo: 'https://handlers.education.launchcode.org/static/images/guion-bluford.jpg', born: 'November 22, 1942', died: 'N/A'},
+    {name: "Kjell Lindgren", photo: 'https://handlers.education.launchcode.org/static/images/kjell-lindgren.jpg', born: 'January 23, 1973', died: 'N/A'},
+    {name: "Jeanette Epps", photo: 'https://handlers.education.launchcode.org/static/images/jeanette-epps.jpg', born: 'November 3, 1970', died: 'N/A'}
   ];
 
   constructor() { }
--- src/app/experiments/experiments.component.ts
+++ src/app/experiments/experiments.component.ts
@@ -8,8 +8,33 @@ import { Component, OnInit } from '@angular/core';
 export class ExperimentsComponent implements OnInit {
    experiments: string[] = ['Moon soil sample', 'Plant growth in orbit', 'Human bone density changes', 'Water droplet podcast for grades K-5', 'Satellite launch'];
 
+   currentExperiments: string[] = [];
+
+   message: string = "";
+
    constructor() { }
 
    ngOnInit() { }
+
+   addExperiment(experiment: string) {
+    if(this.currentExperiments.includes(experiment)) {
+      this.currentExperiments.splice(this.currentExperiments.indexOf(experiment),1);
+    } else {
+      this.currentExperiments.push(experiment);
+    }
+   }
+
+   new(experiment: string) {
+    if(experiment === "") {
+      this.message = "Please enter an experiment name!";
+      return;
+    }
+    if(this.experiments.includes(experiment)) {
+      this.message = `Experiment ${experiment} already on list!`;
+      return;
+    }
+    this.experiments.push(experiment);
+    this.message = "";
+   }
 
 }
